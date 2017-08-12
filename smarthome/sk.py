2dcbe3a2e1b1edad0b1d0de3695a541623d8804a
topsai <hurte@foxmail.com>
2017-08-12 19:11:16 +0800

    test

--- smarthome/sk.py
+++ smarthome/sk.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
 
+from django_alexa.api import fields, intent, ResponseBuilder
 
-import socket
+HOUSES = ("gryffindor", "hufflepuff", "ravenclaw", "slytherin")
 
-sk = socket.socket()
-sk.bind(('0.0.0.0', 443))
-sk.listen(1)
-conn, address = sk.accept()
-print('IP:{}, PORT:{}'.format(*address))
-conn.sendall('welcome'.encode())
-while True:
-    user_input = conn.recv(8096).decode()
-    print(user_input)
+@intent
+def LaunchRequest(session):
+    """
+    Hogwarts is a go
+    ---
+    launch
+    start
+    run
+    begin
+    open
+    """
+    return ResponseBuilder.create_response(message="Welcome to Hog warts school of witchcraft and wizardry!",
+                                           reprompt="What house would you like to give points to?",
+                                           end_session=False,
+                                           launched=True)
+
+
+class PointsForHouseSlots(fields.AmazonSlots):
+    house = fields.AmazonCustom(label="HOUSE_LIST", choices=HOUSES)
+    points = fields.AmazonNumber()
+
+
+@intent(slots=PointsForHouseSlots)
+def AddPointsToHouse(session, house, points):
+    """
+    Direct response to add points to a house
+    ---
+    {points} {house}
+    {points} points {house}
+    add {points} points to {house}
+    give {points} points to {house}
+    """
+    kwargs = {}
+    kwargs['message'] = "{0} points added to house {1}.".format(points, house)
+    if session.get('launched'):
+        kwargs['reprompt'] = "What house would you like to give points to?"
+        kwargs['end_session'] = False
+        kwargs['launched'] = session['launched']
+    return ResponseBuilder.create_response(**kwargs)
