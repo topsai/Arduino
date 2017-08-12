4b0f15ff54e345710876041636444992a7288fc0
topsai <hurte@foxmail.com>
2017-08-12 16:41:36 +0800

    test

--- smarthome/views.py
+++ smarthome/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 # Create your views here.
-
-
+post = []
+get = []
 def index(request):
     # test
     if request.method =='POST':
         data = request.POST
+        post.append(data)
     else:
         data = request.GET
-    print(data)
-    return render(request, 'index.html', {'data': data})
+        get.append(data)
+    return render(request, 'index.html', {'post': post, 'get': get})
 
 
 
