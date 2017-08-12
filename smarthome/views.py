100154c4561858d48e6622c98c5f73b722d8da5c
topsai <hurte@foxmail.com>
2017-08-12 16:46:25 +0800

    test

--- smarthome/views.py
+++ smarthome/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import json
+
 # Create your views here.
-post = []
-get = []
+
+
 def index(request):
     # test
-    if request.method =='POST':
-        data = request.POST
-        post.append(data)
-    else:
-        data = request.GET
-        get.append(data)
-    return render(request, 'index.html', {'post': post, 'get': get})
-
-
-
-
+    d = {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {"type": "PlainText", "text": "Text to speak back to the user."},  # text中是语音回复的内容
+            "card": {  # 显示在Alexa APP中的内容
+                "type": "Simple",
+                "title": "Example of the Card Title",
+                "content": "Example of card content. This card has just plain text content.\n"
+                           "The content is formatted with line breaks to improve readability."
+            }
+        }
+    }
+    return HttpResponse(json.dumps(d))
