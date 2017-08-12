b332b8b1ee7384e6518051d7109574f6f9dffb92
topsai <hurte@foxmail.com>
2017-08-12 17:55:29 +0800

    test

--- smarthome/sk.py
+++ smarthome/sk.py
@@ -8,7 +8,7 @@ sk = socket.socket()
 sk.bind(('0.0.0.0', 443))
 sk.listen(1)
 conn, address = sk.accept()
-print('客户端接入，IP：{}，端口：{}'.format(*address))
+print('IP:{}, PORT:{}'.format(*address))
 conn.sendall('welcome'.encode())
 while True:
     user_input = conn.recv(8096).decode()
