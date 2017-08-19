f76e350d2801e97bd3b0ce5f00cbd72aece6902b
topsai <hurte@foxmail.com>
2017-08-19 21:40:51 +0800

    test

--- django_alexa/alexa_app_setting.py
+++ django_alexa/alexa_app_setting.py
@@ -3,7 +3,7 @@
 from django_alexa.internal.intents_schema import IntentsSchema
 
 ALEXA_APP_ID_DEFAULT = "amzn1.ask.skill.c916f111-fe9e-4b9a-9fe4-33ea7b30eeb9"
-ALEXA_APP_ID_OTHER = "Operatesomething"  # "Your Amazon Alexa App ID OTHER"  # for each app
+ALEXA_APP_ID_OTHER = "light"  # "Your Amazon Alexa App ID OTHER"  # for each app
 ALEXA_REQUEST_VERIFICATON = False  # Enables/Disable request verification
 
 
