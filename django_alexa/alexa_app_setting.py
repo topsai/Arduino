b1b6a4ef0bd686cca706bae9288d5536e53bbd92
topsai <hurte@foxmail.com>
2017-08-19 20:19:20 +0800

    test

---
+++ django_alexa/alexa_app_setting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+
+
+ALEXA_APP_ID_DEFAULT = "amzn1.ask.skill.c916f111-fe9e-4b9a-9fe4-33ea7b30eeb9"
+ALEXA_APP_ID_OTHER = []  # "Your Amazon Alexa App ID OTHER"  # for each app
+ALEXA_REQUEST_VERIFICATON = False  # Enables/Disable request verification
