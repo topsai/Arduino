5dd3b8bd85d56520618f4fe137a514751b08c073
topsai <hurte@foxmail.com>
2017-08-20 21:07:17 +0800

    update

--- django_alexa/alexa.py
+++ django_alexa/alexa.py
@@ -101,7 +101,7 @@ def Operatesomething(session, device, status, ):
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
     all_device = r.get('all_device')
     import json
-    dd = json.loads(all_device.decode())
+    dd = eval(all_device.decode())
     print(dd, type(dd))
     from channels import Group, channel
     # channel.Channel(message.reply_channel.name).send({'text': 'ok'})
