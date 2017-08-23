2af122009067bcd05c6c56b9e6c0d743acebc6e2
topsai <hurte@foxmail.com>
2017-08-23 19:10:38 +0800

    update

--- Arduino/asgi.py
+++ Arduino/asgi.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*- 
 
 
-import os
+import os, sys
+# sys.path.append('/usr/local/virtunlenvs/Arduino/Arduino')
+sys.path.append('/usr/local/virtunlenvs/Arduino')
+# sys.path.append('/home/banjer') # this line solved it
+sys.executable = '/usr/local/virtunlenvs/Arduino/virtual_evn/bin/python'
+
+
 from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Arduino.settings")
